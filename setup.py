185111fa30d737b7d038c5538289849b6c1d53a7
Armon Dadgar <armon.dadgar@gmail.com>
2011-08-28 23:10:30 -0700

    Added setup file

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, Command
+import bloomd
+
+# Get the long description by reading the README
+try:
+    readme_content = open("README.rst").read()
+except:
+    readme_content = ""
+
+# Create the actual setup method
+setup(name='bloomd',
+      version=bloomd.__version__,
+      description='Lightweight server to manage probabilistic sets based on bloom filters',
+      long_description=readme_content,
+      author='Armon Dadgar',
+      author_email='armon@kiip.me',
+      maintainer='Armon Dadgar',
+      maintainer_email='armon@kiip.me',
+      url="https://github.com/kiip/bloomd/",
+      license="MIT License",
+      keywords=["bloom", "filter","server","twisted"],
+      packages=['bloomd'],
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Database",
+        "Topic :: Internet",
+    ]
+      )
